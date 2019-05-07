fee192c7a1538298476e7d830aecab5da69f5d94
Lemon <1730392988@qq.com>
2019-05-07 16:32:56 +0800

    user注册 修改

--- jushi-module/jushi-module-user/src/main/java/com/jushi/user/JuShiUserApplication.java
+++ jushi-module/jushi-module-user/src/main/java/com/jushi/user/JuShiUserApplication.java
@@ -3,15 +3,20 @@ package com.jushi.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @ComponentScan(basePackages = {"com.jushi.security.client","com.jushi.user"})
 public class JuShiUserApplication {
-
+    @Bean
+    public BCryptPasswordEncoder bcryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
     public static void main(String[] args)
     {
         SpringApplication.run(JuShiUserApplication.class, args);
